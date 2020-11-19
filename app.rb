dd8ae2cd38120db3d3d4c7f8aff083600b8ac169
mbish08 <mdrew8@gmail.com>
2020-11-18 17:20:06 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        erb :user_input
+    end 
+
+    post '/piglantinize' do
+        @piglatin = Piglatinizer.new.piglantinize(params[:user_phrase]) 
+        erb :results        
+    end 
+
 end
